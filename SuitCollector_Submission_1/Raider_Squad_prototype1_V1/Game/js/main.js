384afc2e99891ef93cb08e9db660e5d0b5a5a166
James Ballari <james.q917@gmail.com>
2023-03-22 21:34:26 +0530

    Updates few files

---
+++ SuitCollector_Submission_1/Raider_Squad_prototype1_V1/Game/js/main.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    
+    var board = [11,12,13,14,21,22,23,24,31,32,33,34,41,42,43,44];
+    var suites = [-1,-1]
+
+    function shuffleArray(array) { 
+        return array.sort( ()=>Math.random()-0.5 );
+    } 
+
+
+});
